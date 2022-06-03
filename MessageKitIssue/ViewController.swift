3cd0e211d0b3bba7f6084aa0aa21c30581b00204
Denysov Illia <illia.denysov@helplightning.com>
2022-06-03 16:02:27 +0200

    Initial Commit

---
+++ MessageKitIssue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageKitIssue
+//
+//  Created by Denysov Illia on 03.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
